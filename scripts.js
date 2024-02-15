9d05644db8adddf7296f280dd39c7a180a61acb0
Gamen604 <122073994+Gamen604@users.noreply.github.com>
2024-02-15 19:25:10 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const socialMediaBtn = document.getElementById("socialMediaBtn");
+const socialMediaPanel = document.createElement("div");
+socialMediaPanel.id = "socialMediaPanel";
+socialMediaPanel.className = "panel";
+socialMediaPanel.innerHTML = `
+  <button class="socialMediaBtn">Instagram</button>
+  <button class="socialMediaBtn">Twitter</button>
+  <button class="socialMediaBtn">Youtube</button>
+`;
+socialMediaBtn.appendChild(socialMediaPanel);
+
+socialMediaPanel.style.position = "absolute";
+socialMediaPanel.style.top = "100%";
+socialMediaPanel.style.display = "none";
+
+socialMediaBtn.addEventListener("click", () => {
+  if (socialMediaPanel.style.display === "none") {
+    socialMediaPanel.style.display = "block";
+  } else {
+    socialMediaPanel.style.display = "none";
+  }
+});
